7baf3dcdbcf8b879e73e2028e9d1fc22366cde54
jfsenechal <jfsenechal@gmail.com>
2023-08-08 21:47:43 +0200

    add

---
+++ src/Doctrine/PropertyUtil.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace AfmLibre\Pathfinder\Doctrine;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bridge\Doctrine\PropertyInfo\DoctrineExtractor;
+use Symfony\Component\PropertyAccess\PropertyAccess;
+use Symfony\Component\PropertyAccess\PropertyAccessor;
+
+class PropertyUtil
+{
+    public function __construct(private EntityManagerInterface $entityManager)
+    {
+    }
+
+    public function getProperties(string $className): ?array
+    {
+        $doctrineExtractor = new DoctrineExtractor($this->entityManager);
+
+        return $doctrineExtractor->getProperties($className);
+    }
+
+    public function getPropertyAccessor(): PropertyAccessor
+    {
+        return PropertyAccess::createPropertyAccessor();
+    }
+}
---
+++ src/Doctrine/SetUserAddSubscriber.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace AfmLibre\Pathfinder\Doctrine;
+
+use Doctrine\Bundle\DoctrineBundle\Attribute\AsDoctrineListener;
+use Doctrine\ORM\Events;
+use Doctrine\Persistence\Event\LifecycleEventArgs;
+use Exception;
+use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+#[AsDoctrineListener(event: Events::prePersist)]
+final class SetUserAddSubscriber
+{
+    public function __construct(private Security $security, private PropertyUtil $propertyUtil)
+    {
+    }
+
+    public function prePersist(LifecycleEventArgs $lifecycleEventArgs): void
+    {
+        $object = $lifecycleEventArgs->getObject();
+        if (!$this->propertyUtil->getPropertyAccessor()->isWritable($object, 'userAdd')) {
+            return;
+        }
+
+        $this->setUserAdd($object);
+    }
+
+    private function setUserAdd(object $entity): void
+    {
+        //for loading fixtures
+        if ($entity->getUserAdd()) {
+            return;
+        }
+
+        $user = $this->security->getUser();
+
+        if (!$user instanceof UserInterface) {
+            throw new Exception('You must be login');
+        }
+
+        if ($user) {
+            $entity->setUserAdd($user->getUserIdentifier());
+        }
+    }
+}
