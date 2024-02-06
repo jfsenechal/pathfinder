1dcb4e1cb2b9b11012a73c5387b11a86ea408595
jfsenechal <jfsenechal@gmail.com>
2024-02-06 17:12:30 +0100

    up

--- config/packages/twig.php
+++ config/packages/twig.php
@@ -4,7 +4,8 @@ use Symfony\Config\TwigConfig;
 
 return static function (TwigConfig $twig) {
     $twig
-        ->path('%kernel.project_dir%/src/AfmLibre/Pathfinder/templates', 'AfmLibrePathfinder')->formThemes(
+        ->path('%kernel.project_dir%/src/AfmLibre/Pathfinder/templates', 'AfmLibrePathfinder')
+        ->formThemes(
             ['bootstrap_5_layout.html.twig']
         );
 };
